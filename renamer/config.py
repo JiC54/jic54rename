cac4efeae614ca93657151fddab0a5686cec61a4
Sourcery AI <>
2022-01-04 01:11:11 +0000

    'Refactored by Sourcery'

--- renamer/config.py
+++ renamer/config.py
@@ -3,18 +3,25 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+
+
 class Config:
     API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
     OWNER_ID =  int(os.environ.get("OWNER_ID", ""))
-    AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS", "") else []
+    AUTH_USERS = (
+        [int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")]
+        if os.environ.get("AUTH_USERS", "")
+        else []
+    )
+
     if OWNER_ID not in AUTH_USERS:
         AUTH_USERS.append(OWNER_ID)
     BANNED_USERS = [int(i) for i in os.environ.get("BANNED_USERS", "").split(" ")] if os.environ.get("BANNED_USERS", "") else None
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    BOT_PASSWORD = os.environ.get("BOT_PASSWORD", "") if os.environ.get("BOT_PASSWORD", "") else None
+    BOT_PASSWORD = os.environ.get("BOT_PASSWORD", "") or None
     CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION") if os.environ.get("CUSTOM_CAPTION", "") else None
-    FORCE_SUB = os.environ.get("FORCE_SUB", "") if os.environ.get("FORCE_SUB", "") else None
+    FORCE_SUB = os.environ.get("FORCE_SUB", "") or None
     DB_URI = os.environ.get("DB_URI", "")
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     try:
